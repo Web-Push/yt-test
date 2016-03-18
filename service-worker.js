fd07b94fd153f4e06644b1e4c034c9f9cf353383
yt-harada <yt-harada@kddi-tech.com>
2016-03-18 14:52:22 +0900

    アイコン

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('push', function(event) {
   console.log('Received a push message', event);
   var title = 'Yay a message.';
   var body = 'We have received a push message.';
-  var icon = '/images/icon-192x192.png';
+  var icon = '/icon.png';
   var tag = 'simple-push-demo-notification-tag';
 
   event.waitUntil(
