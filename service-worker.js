c71c43b6812ebd348ef975d3e99443c7c4416825
yt-harada <yt-harada@kddi-tech.com>
2016-03-25 09:33:04 +0900

    fetchのお試し

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-
+/** Webサイト側からPostMessageで送られたデータを受信する */
 self.addEventListener('message', function (event) {
   console.log(event.data.action);
   console.log(event.data.userId);
@@ -7,11 +7,22 @@ self.addEventListener('message', function (event) {
   writeDB(event.data.userId, event.data.serviceUrl);
 });
 
-
+/** Push通知を受けたときの処理 */
 self.addEventListener('push', function(event) {
   console.log('Received a push message', event);
-  checkLogin();
 
+  fetch('https://web-push.github.io/yt-test/users.json').then(function(response){
+    if (response.status !== 200) {
+      console.log('Looks like there was a problem. Status Code: ' + response.status);
+    } else {
+      response.text().then(function(textdata) {
+        console.log('text:' + textdata);
+        var contact = JSON.parse(jsontext);
+        console.log('JSON:' + contact);
+      })
+    }
+  });
+/*
   var title = 'yt-test.';
   var body = 'yt-test のページで登録したServiceWorkerです。';
   var icon = '/images/icon-192x192.png';
@@ -24,8 +35,10 @@ self.addEventListener('push', function(event) {
       tag: tag
     })
   );
+*/
 });
 
+/** Notificationをクリックしたときの処理 */
 self.addEventListener('notificationclick', function(event) {
   console.log('On notification click: ', event.notification.tag);
   // Android doesn’t close the notification when you click on it
@@ -49,6 +62,7 @@ self.addEventListener('notificationclick', function(event) {
   }));
 });
 
+/** ServiceWorkerのactivateが完了したときの処理 */
 self.addEventListener('activate', function(event) {
   createDB();
   event.waitUntil(self.clients.claim());
@@ -56,42 +70,35 @@ self.addEventListener('activate', function(event) {
 });
 
 
-
+/** indexDBの生成処理 */
 function createDB(){
+  // chrome以外のブラウザで試すときは[var database = indexedDB]の定義を変えないと多分動かない
   var database = indexedDB;
   var db = null;
 
   if (database) {
     database.deleteDatabase("mydb");
-
     var openRequest = database.open("mydb", 1.0);
-    
     openRequest.onupgradeneeded = function(event) {
       // データベースのバージョンに変更があった場合(初めての場合もここを通ります。)
       db = event.target.result;
       var store = db.createObjectStore("books", { keyPath: "mykey"});
-     
       // インデックスを作成します。
       store.createIndex("myvalueIndex", "myvalue");
     }
-    
+
     openRequest.onsuccess = function(event) {
       db = event.target.result;
     }
   }
 }
 
+/** indexDBへ書き込み処理（書き方か読み方を変えないと面倒） */
 function writeDB(user, url){
-  //データ作成
-  var userdata = {
-    user: user,
-    url: url
-  };
-  
   var database = indexedDB;
   var req = database.open("mydb");
   var db = null;
-  
+
   //成功時コールバック
   req.onsuccess = function(evt) {
     db = evt.target.result;
@@ -104,6 +111,7 @@ function writeDB(user, url){
   }
 }
 
+/** indexDBに登録されているデータとfetchしてきたデータとのマッチング */
 function checkLogin() {
   var database = indexedDB;
   var req = database.open("mydb");
